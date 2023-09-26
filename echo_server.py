6fbb4ec23c6b4606c76dde4b79be777026d1cde9
fzhu886 <fzhu@ualberta.ca>
2023-09-25 22:17:24 -0600

    adding echo_server.py to the repo

--- echo_server.py
+++ echo_server.py
@@ -1,33 +1,40 @@
-#!/usr/bin/env python3
 import socket
-import time
+from threading import Thread
 
-#define address & buffer size
-HOST = ""
-PORT = 8001
-BUFFER_SIZE = 1024
+BYTES_TO_READ = 4096
+HOST = "127.0.0.1" #IP #localhost
+PORT = 8080
 
-def main():
+def handle_connection(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(BYTES_TO_READ) # wait for a request, and when you get it, you receive it
+            if not data: # if I receive an empty byte string ==> b''
+                break
+            print(data)
+            conn.sendall(data) # send it back to the client
+
+# Start single threaded echo server
+def start_server():
+    with socket.socket(sockey.AF_INET, socket.SOCK_STREAM) as s: # initialize the socket
+        s.bind((HOST, PORT)) # bind to ip and port
+        s.setsockopt(socket.SOL_SOCKET, socket, SO_REUSEADDR, 1) # Ignore this for now => set reuseaddr to 1
+        s.listen() # listen for incoming connections
+        conn, addr = s.accept() # conn = socket referring to the client, addr => address of the client [IP, Port]
+        handle_connection(conn, addr) # send it a response
+
+# Start multithreaded echo server
+def start_threaded_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
         s.bind((HOST, PORT))
-        #set to listening mode
-        s.listen(2)
-        
-        #continuously listen for connections
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.listen(2) # Allow backlog of up to 2 connections ==> queue [waiting conn1, waiting conn2]
         while True:
             conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
-            conn.close()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
 
-if __name__ == "__main__":
-    main()
+# Start_server()
+start_threaded_server()
+    
